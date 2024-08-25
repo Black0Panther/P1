c84c9e6dd59b581eeae605bffd74793c099411a0
Manvendra Shekhawat <126936011+Black0Panther@users.noreply.github.com>
2024-08-25 21:03:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const mobile_menu_button = document.getElementById("mobile-menu-button");
+const mobile_menu = document.getElementById("mobile-menu");
+
+console.log("hello");
+
+mobile_menu_button.addEventListener("click",()=>{
+ 
+    mobile_menu.classList.toggle("hidden");
+    
+
+})
+//                <a href="https://www.linkedin.com/in/manvendra-s558811/"target="_blank"><img src="images/linkdinlgo.png" alt="" class="w-8 h-8"></a>
